5f73c8ab5522874929d7cc423818e37e5b4054cb
Christia T <christiatidwell@gmail.com>
2018-12-02 22:16:51 +0000

    changed creation of new dentist to avoid blank sign on

--- app/controllers/dentist_controller.rb
+++ app/controllers/dentist_controller.rb
@@ -5,10 +5,13 @@ class DentistController < ApplicationController
   end
 
   post '/dentists' do
-    @dentist = Dentist.new
-    @dentist.email = params[:email]
-    @dentist.password = params[:password]
-    if @dentist.save
+    # @dentist = Dentist.new
+    # @dentist.email = params[:email]
+    # @dentist.password = params[:password]
+    # if @dentist.save
+    if params[:email] != "" && params[:password] != ""
+      Dentist.create(params)
+      #binding.pry
       redirect '/patients'
     else
       erb :'dentists/new'
